be6c8a271ab3fcc7ae985aeddfd56631405074f4
Katherina <mnogougolnichek@gmail.com>
2017-08-26 19:49:52 +0300

    Update main.js

--- ArkeneaProj/scripts/main.js
+++ ArkeneaProj/scripts/main.js
@@ -2,40 +2,38 @@
 
 document.addEventListener("DOMContentLoaded", setup);
 
-function setup(){
-    (()=>{
-        let swiper = new Swiper('.swiper-container', {
+function setup() {
+    (function () {
+        var swiper = new Swiper('.swiper-container', {
             pagination: '.swiper-pagination',
             paginationClickable: true,
             autoplay: true,
             autoplayStopOnLast: false
         });
 
-        let trigger = document.querySelector('.trigger'),
-         menu = document.querySelector('.nav-list'),
-         headerContent = document.querySelector('.header-content-wrapper'),
-         headerForm = document.querySelector('.header-form');
+        var trigger = document.querySelector('.trigger'),
+            menu = document.querySelector('.nav-list'),
+            headerContent = document.querySelector('.header-content-wrapper'),
+            headerForm = document.querySelector('.header-form');
 
-
-       
-        document.addEventListener('click', event => {
-         event.preventDefault();
-         let el = event.target;
-         if(el === trigger) {
-             menu.classList.toggle('on');
-             headerContent.classList.toggle('off');
-             headerForm.classList.toggle('off');
-         }else if(!el.classList.contains('nav-link')){
-             menu.classList.remove('on');
-             headerContent.classList.remove('off');
-             headerForm.classList.remove('off');
-         };
+        document.addEventListener('click', function (event) {
+            event.preventDefault();
+            var el = event.target;
+            if (el === trigger) {
+                menu.classList.toggle('on');
+                headerContent.classList.toggle('off');
+                headerForm.classList.toggle('off');
+            } else if (!el.classList.contains('nav-link')) {
+                menu.classList.remove('on');
+                headerContent.classList.remove('off');
+                headerForm.classList.remove('off');
+            };
         }, false);
 
-        let deviceClass = isTouch()===true&&(document.querySelector("body").classList.add("touch"));
+        var deviceClass = isTouch() === true && document.querySelector("body").classList.add("touch");
 
         function isTouch() {
-            return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+            return 'ontouchstart' in window || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
         };
     })();
 };
