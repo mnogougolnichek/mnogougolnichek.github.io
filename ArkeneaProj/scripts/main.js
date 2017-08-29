3a834285278924f95bd577f47eca77b4c98c38a1
Katherina <mnogougolnichek@gmail.com>
2017-08-29 18:00:50 +0300

    Update main.js

--- ArkeneaProj/scripts/main.js
+++ ArkeneaProj/scripts/main.js
@@ -32,10 +32,6 @@ function setup() {
             };
        });
 
-        var deviceClass = isTouch() === true && document.querySelector("body").classList.add("touch");
 
-        function isTouch() {
-            return 'ontouchstart' in window || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-        };
     })();
 };
