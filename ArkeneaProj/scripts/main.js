39090fe9e3acb921829ad900e762c1a83a17e641
Katherina <mnogougolnichek@gmail.com>
2017-08-29 18:20:02 +0300

    Update main.js

--- ArkeneaProj/scripts/main.js
+++ ArkeneaProj/scripts/main.js
@@ -12,16 +12,21 @@ function setup() {
         });
 
         var trigger = document.querySelector('.trigger'),
-            menu = document.querySelector('.nav-list');
+            menu = document.querySelector('.nav-list'),
+            headerContent = document.querySelector('.header-content-wrapper'),
+            headerForm = document.querySelector('.header-form');
 
-               document.addEventListener('click', function (event) {
+        document.addEventListener('click', function (event) {
             event.preventDefault();
             var el = event.target;
             if (el === trigger) {
                 menu.classList.toggle('on');
-
+                headerContent.classList.toggle('off');
+                headerForm.classList.toggle('off');
             } else if (!el.classList.contains('nav-link')) {
                 menu.classList.remove('on');
+                headerContent.classList.remove('off');
+                headerForm.classList.remove('off');
             };
         }, false);
 
