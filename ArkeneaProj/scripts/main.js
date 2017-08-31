740ddd2c0ab484a2072f1fed32863915bb7b3059
Katherina <mnogougolnichek@gmail.com>
2017-08-31 19:30:11 +0300

    Update main.js

--- ArkeneaProj/scripts/main.js
+++ ArkeneaProj/scripts/main.js
@@ -5,10 +5,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
+    var trigger = document.querySelector('.trigger'),
+            menu = document.querySelector('.nav-list'),
+            headerContent = document.querySelector('.header-content-wrapper'),
+            headerForm = document.querySelector('.header-form');
 
-alert('444');
 
-
+    document.addEventListener('click', function (event) {
+        event.preventDefault();
+        var el = event.target;
+        if (el === trigger) {
+            menu.classList.toggle('on');
+           
+        } 
+    }, false);
 
 
 
