ac8b2a24f39375504918c4b79960a18aacfe505f
mnogougolnichek <mnogougolnichek@gmail.com>
2018-10-04 20:10:52 +0300

    datatable

--- datatable/src/reducers/setCurrentPage.js
+++
@@ -1,8 +0,0 @@
-const initialState = 0;
-
-export default function setCurrentPage(state = initialState, action) {
-    if(action.type === 'SET_CURRENT_PAGE'){
-        return action.payload
-    }
-    return state;
-}
