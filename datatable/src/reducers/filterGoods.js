ea5352d53efb53ae6a30a1a20d23ec428c2dd05f
mnogougolnichek <mnogougolnichek@gmail.com>
2018-10-05 00:07:48 +0300

    delete datatable

--- datatable/src/reducers/filterGoods.js
+++
@@ -1,8 +0,0 @@
-const initialState = [];
-
-export default function filterGoods(state = initialState, action) {
-    if(action.type === 'FILTER_GOODS'){
-        return  action.payload;
-    }
-    return state;
-}
