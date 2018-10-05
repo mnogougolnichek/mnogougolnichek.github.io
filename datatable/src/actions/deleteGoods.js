8a08a8832203508a632d87f2eb52e38e06a0d972
mnogougolnichek <mnogougolnichek@gmail.com>
2018-10-05 20:05:20 +0300

    dt delete

--- datatable/src/actions/deleteGoods.js
+++
@@ -1,5 +0,0 @@
-
-export const deleteGoods = (prevGoods, delItem) => dispatch =>{
-    let nextGoods = prevGoods.filter(item => item.id !== delItem);
-    dispatch({type: 'DELETE_GOODS', payload: nextGoods});
-};
--- datatable/src/reducers/setFilter.js
+++
@@ -1,9 +0,0 @@
-const initialState = '';
-
-export default function setFilter(state = initialState, action) {
-    if(action.type === 'SET_FILTER'){
-        return  action.payload;
-
-    }
-    return state;
-}
